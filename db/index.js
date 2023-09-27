0cf56eff84788a4ea279a4045cea7b9f890621e7
Jorly Joseph <jorlymail@gmail.com>
2023-09-27 23:05:55 +0200

    login routes added

--- db/index.js
+++ db/index.js
@@ -1,12 +1,12 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/project-management-server";
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/books-library-project';
 
 mongoose
   .connect(MONGO_URI)
@@ -15,5 +15,5 @@ mongoose
     console.log(`Connected to Mongo! Database name: "${dbName}"`);
   })
   .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
+    console.error('Error connecting to mongo: ', err);
   });
--- models/User.model.js
+++ models/User.model.js
@@ -3,6 +3,9 @@ const { Schema, model } = require('mongoose');
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    name: {
+      type: String
+    },
     email: {
       type: String,
       required: [true, 'Email is required.'],
